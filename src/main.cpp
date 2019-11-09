63daf6b39fba3284d8746174845bb81651d67750
IcE <ivica-matic@outlook.com>
2019-11-09 01:25:42 +0000

    Added libraries to workspace

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include "dummyclass.h"
+#include <fmt/format.h>
+#include <sqlite3.h>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     dummyclass a;
+    std::string s = fmt::format("I'd rather be {1} than {0}.\n", "right", "happy");
+    std::cout<<s;
+    sqlite3 *db;
+    sqlite3_open("database.db", &db);
+
+    // create an empty structure (null)
+    json j;
+    j["pi"];
+    j["pi"][0] = 2;
+    j["pi"][1] = 4;
+
+    std::cout<<j["pi"][1];
     return 0;
 }
