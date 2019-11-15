01d8dc625f868a2fb136b52e0a77c5d57f13ae1c
Cian Payne <cianjpayne@gmail.com>
2019-11-15 02:07:19 +0000

    Resolved merge conflict

---
+++ include/exceptions.h
@@ -0,0 +1,35 @@
+//
+// Created by 22ru on 11/15/19.
+//
+
+#ifndef DATASET_LABELING_TOOL_EXCEPTIONS_H
+#define DATASET_LABELING_TOOL_EXCEPTIONS_H
+
+#include <bits/exception.h>
+#include <stdexcept>
+
+class ValueNotFoundError : public std::exception
+{
+    const char * what () const throw ()
+    {
+        return "Error: Value not found in array";
+    }
+};
+
+class IndexOutOfBoundsError : public std::exception
+{
+    const char * what () const throw ()
+    {
+        return "Error: Array index is out of bounds";
+    }
+};
+
+class ArrayEmptyError : public std::exception
+{
+    const char * what () const throw ()
+    {
+        return "Error: Array is empty";
+    }
+};
+
+#endif //DATASET_LABELING_TOOL_EXCEPTIONS_H
