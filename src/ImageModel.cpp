97ecb2ca01da91cca80312a2322b76fd40d44212
Cian Payne <cianjpayne@gmail.com>
2020-02-13 17:34:55 +0000

    Made ImageModel method stubs

---
+++ src/ImageModel.cpp
@@ -0,0 +1,31 @@
+#include "ImageModel.h"
+
+void ImageModel::browseFolder()
+{
+    // TODO
+}
+
+LinkedList<std::string> ImageModel::searchLoaded(const std::string& pathName)
+{
+    // TODO
+}
+
+std::pair<int, int> ImageModel::getResolution(const std::string& pathName)
+{
+    // TODO
+}
+
+long ImageModel::getFileSize(const std::string& pathName)
+{
+    // TODO
+}
+
+LinkedList<std::string> ImageModel::getAll()
+{
+    // TODO
+}
+
+QImage ImageModel::getImage(const std::string& pathName)
+{
+    // TODO
+}
