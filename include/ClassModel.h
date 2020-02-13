be7097dff4e3c4937827fcc09a31ab367bf4d513
Cian Payne <cianjpayne@gmail.com>
2020-02-13 17:47:13 +0000

    Wrote ClassModel header

---
+++ include/ClassModel.h
@@ -0,0 +1,24 @@
+#include <string>
+#include <QFileSystemModel>
+
+#include <LinkedList.h>
+
+class ClassModel
+{
+private:
+    QFileSystemModel fileSystemModel;
+    LinkedList<std::string> supportedFileFormats;
+    std::string selectedClass;
+    std::string currentFilePath;
+
+    void save();
+
+public:
+    void browse();
+    void create(const std::string&);
+    void addClass(const std::string&);
+    void removeClass(const std::string&);
+    void select(const std::string&);
+    std::string getSelected();
+    LinkedList<std::string> getAll();
+};
