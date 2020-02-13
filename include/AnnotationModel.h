a31973cb29d5e81ba8ffcc053fe0560f618a2368
Cian Payne <cianjpayne@gmail.com>
2020-02-13 18:08:11 +0000

    Wrote AnnotationModel header file

---
+++ include/AnnotationModel.h
@@ -0,0 +1,23 @@
+#include <string>
+#include <utility>
+#include <QFileSystemModel>
+
+#include "LinkedList.h"
+
+using Point = std::pair<int, int>;
+using Shape = LinkedList<Point>;
+
+class AnnotationModel
+{
+private:
+    QFileSystemModel fileSystemModel;
+    LinkedList<std::string> supportedFileFormats;
+    std::string currentFilePath;
+
+    void save();
+
+public:
+    void browse();
+    void create(const std::string&);
+    void add(const std::string&, const int, const LinkedList<Shape>&);
+};
