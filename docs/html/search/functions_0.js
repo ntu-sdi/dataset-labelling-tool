f83bf578f4c0122d78facc6625df424af29343a1
PetarPeychev <petarpeychev98@gmail.com>
2020-02-13 15:27:58 +0000

    added Doxyfile and generated html docs

---
+++ docs/html/search/functions_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['main',['main',['../main_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main.cpp']]],
+  ['maincontroller',['MainController',['../classMainController.html#afc816e394f59614efba13d0657a0a351',1,'MainController']]],
+  ['mainview',['MainView',['../classMainView.html#a244a9eb314487f94517a386926d51f0f',1,'MainView']]]
+];
