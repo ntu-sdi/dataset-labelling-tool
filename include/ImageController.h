3edfaaff6edba2418f002073034487914253e038
Cian Payne <ciantheskier@gmail.com>
2020-02-15 18:36:58 +0000

    Wrote header for ImageController

--- include/ImageController.h
+++ include/ImageController.h
@@ -1,8 +1,16 @@
 #include "MainView.h"
+#include "ImageModel.h"
 
 class ImageController
 {
 private:
     Ui::MainView ui;
+    ImageModel model;
 
+public:
+    void browse();
+    LinkedList<std::string> searchLoaded(const std::string&);
+    LinkedList<std::string> sortLoaded();
+    std::string select(const std::string&);
+    void open();
 };
