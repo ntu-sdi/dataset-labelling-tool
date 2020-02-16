f58283c1d9073c1b15a53d83618511e6b94d4dba
Cian Payne <cianjpayne@gmail.com>
2020-02-16 15:48:31 +0000

    Wrote header for MainController Added remove and getSelected methods to ClassController header

--- include/ClassController.h
+++ include/ClassController.h
@@ -15,9 +15,6 @@ public:
     LinkedList<std::string> sortLoaded();
     void add(const std::string&);
     void select(const std::string&);
-    /*void open(const std::string&);
-     * This was in the class diagram,
-     * but didn't have match anything in the
-     * sequence diagram.
-     */
+    void remove(const std::string&);
+    std::string getSelected();
 };
--- include/MainController.h
+++ include/MainController.h
@@ -4,6 +4,13 @@
 #include "ClassController.h"
 #include "ImageController.h"
 
+/*
+ * TODO: seeing as the controllers just update the view, do controller
+ * methods even need return types? The class diagram does not specify,
+ * so for now all controller methods have return types but this may be changed
+ * to void later.
+ */
+
 class MainController
 {
 private:
@@ -11,6 +18,27 @@ private:
     ClassController classController;
     ImageController imageController;
 
+    std::string getSelectedClass();
+
 public:
     MainController(AnnotationController&, ClassController&, ImageController&);
+
+    void browseImages();
+    LinkedList<std::string> searchLoadedImages(const std::string&);
+    LinkedList<std::string> sortLoadedImages();
+    std::string selectImage(const std::string&);
+    void openImage();
+
+    void browseForClassFile();
+    LinkedList<std::string> sortLoadedClasses();
+    void createClassFile(const std::string&);
+    void addClass(const std::string&);
+    void selectClass(const std::string&);
+    void removeClass(const std::string&);
+
+    void browseForAnnotationFile();
+    void createAnnotationFile(const std::string&);
+    void addPoint(Point);
+    void finishShape();
+    void cancelShape();
 };
