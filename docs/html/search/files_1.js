7a3d528a121a18bf00ffc38779544fc72bae89b7
IcE <ivica-matic@outlook.com>
2020-03-03 19:30:50 +0000

    Updated gitignore to exclude docs [again]

--- docs/html/search/files_1.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['classcontroller_2ecpp',['ClassController.cpp',['../ClassController_8cpp.html',1,'']]],
-  ['classcontroller_2eh',['ClassController.h',['../ClassController_8h.html',1,'']]],
-  ['classmodel_2ecpp',['ClassModel.cpp',['../ClassModel_8cpp.html',1,'']]],
-  ['classmodel_2eh',['ClassModel.h',['../ClassModel_8h.html',1,'']]]
-];
--- docs/html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['supported_5fformats',['SUPPORTED_FORMATS',['../ImageModel_8h.html#a70ac0f05c956034af538a7f7cd2f0b63',1,'ImageModel.h']]]
-];
