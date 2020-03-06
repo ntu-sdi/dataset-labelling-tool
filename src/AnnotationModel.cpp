05758bbe05ce8597053623ea12fb30de057d8de4
IcE <ivica-matic@outlook.com>
2020-03-06 07:58:36 +0000

    Finished with function to store annotations in annotation files

--- src/AnnotationModel.cpp
+++ src/AnnotationModel.cpp
@@ -3,19 +3,18 @@
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <QJsonArray>
-#include <QTextStream>
-#include <iostream>
-#include "exceptions.h"
+#include <QJsonValue>
 
+#include "exceptions.h"
 #include "AnnotationModel.h"
 
 /**
  * @brief Prompts user to navigate to a annotation file on the system
  *
  * if entered path is empty, or file could not be oppened, std::invalid_argument
- * exception  is thrown.
+ * eannotationception  is thrown.
  *
- * if operation is canceled custom OperationCanceled exception is thrown
+ * if operation is canceled custom OperationCanceled eannotationception is thrown
  *
  * Location of opened file is saved to a private currentFilePath attribute
  * in this class
@@ -34,7 +33,7 @@ void AnnotationModel::browse()
         file.close();
     }
     else {
-        throw std::invalid_argument("Could not open file");
+        throw FileNotFoundError();
     }
 }
 
@@ -42,7 +41,7 @@ void AnnotationModel::browse()
  * @brief Provides a way to enter path to annotation file as a parameter (QString)
  *
  * if entered path is empty, or file could not be oppened, std::invalid_argument
- * exception  is thrown.
+ * eannotationception  is thrown.
  *
  * Location of opened file is saved to a private currentFilePath attribute
  * in this class
@@ -57,16 +56,16 @@ void AnnotationModel::browse(const QString& filePath)
         file.close();
     }
     else {
-        throw std::invalid_argument("Could not open file");
+        throw FileNotFoundError();
     }
 }
 /**
  * @brief Prompts the user for a annotation file name. Creates a file using this name and path.
  *
  * if filePath is empty, or file could not be created at wanted location
- * std::invalid argument exception is thrown
+ * std::invalid argument eannotationception is thrown
  *
- * if operation is canceled custom OperationCanceled exception is thrown
+ * if operation is canceled custom OperationCanceled eannotationception is thrown
  *
  * Location of opened file is saved to a private currentFilePath attribute
  * in this class
@@ -94,7 +93,7 @@ void AnnotationModel::create()
  * @brief Provides a way to create annotation file by providing file path as a parameter (QString)
  *
  * if filePath is empty, or file could not be created at wanted location
- * std::invalid argument exception is thrown
+ * std::invalid argument eannotationception is thrown
  *
  * Location of opened file is saved to a private currentFilePath attribute
  * in this class
@@ -113,48 +112,96 @@ void AnnotationModel::create(const QString& filePath)
     this->currentFilePath = path;
 }
 
+/**
+ * @brief Provides a way to save annotation to annotation file
+ *
+ * Function opens and loads all annotation details from annotation file into memory. After that
+ * it creates a new annotation and adds annotation details to it, or it updates previous annotation
+ * if the image was previously annotated in this annotation file.
+ *
+ * Annotation file which is being edited depends on which file is currently selected,
+ * obtained from getCurrentFile function
+ *
+ * If for some reason that file is not available function throws FileNotFoundError.
+ *
+ * @param imageFilePath QString Path to a image for which annotation is being made.
+ * @param className QString Name of a class which is used for annotating when drawing a shape.
+ * @param coordinates QString LinkedList of all points in a annotation shape, in format x,y (std::pair<int,int>).
+ */
 void AnnotationModel::add(const QString& imageFilePath, const QString& className, LinkedList<std::pair<int,int>>& coordinates) {
-    QJsonObject annotationDetails;
-    annotationDetails.insert("class",className);
-    QJsonArray points;
+    QFile jsonFile(getCurrentFilePath());
+    if (!jsonFile.exists()){
+        throw FileNotFoundError();
+    }
+    jsonFile.open(QIODevice::ReadOnly); //open file in read mode
+    QByteArray data = jsonFile.readAll();
+    QJsonDocument json = QJsonDocument::fromJson(data); //create json document from file contents
+    jsonFile.close();
+    QJsonObject allAnnotations = json.object(); //get outer json object
+    QJsonObject newAnnotation; //create new annotation object for storing class name and list of points
+    QJsonArray points; //create array for string points
     for (int i = 0; i < coordinates.length(); i++){
         int x = coordinates.at(i).first;
         int y = coordinates.at(i).second;
         QJsonObject o;
         o.insert("x",x);
         o.insert("y",y);
-        points.append(o);
+        points.append(o); //append object of points to array of points
     }
-    annotationDetails.insert("points",points);
-    QJsonObject annotation;
-    annotation.insert(imageFilePath,annotationDetails);
-    QJsonDocument json;
-    json.setObject(annotation);
-    QFile jsonFile(getCurrentFilePath());
-    jsonFile.open(QIODevice::WriteOnly | QIODevice::Append);
+    newAnnotation.insert("class",className);
+    newAnnotation.insert("points",points); //fill annotation with data
+    QJsonArray annotationsForThisImage = allAnnotations.value(imageFilePath).toArray(); //get array with all previous annotations for this image
+    annotationsForThisImage.append(newAnnotation); //append all previous annotations with new annotation
+    allAnnotations.insert(imageFilePath,annotationsForThisImage); //set changes to outer json object
+    json.setObject(allAnnotations); //create json document from outer object
+    jsonFile.open(QIODevice::WriteOnly); //write contents to file
     jsonFile.write(json.toJson());
     jsonFile.close();
 }
 
+/**
+ * @brief Provides a way to save annotation to annotation file
+ *
+ * Function opens and loads all annotation details from annotation file into memory. After that
+ * it creates a new annotation and adds annotation details to it, or it updates previous annotation
+ * if the image was previously annotated in this annotation file.
+ *
+ * Annotation file which is being edited is passed as 1st parameter to this function.
+ *
+ * If for some reason that file is not available function throws FileNotFoundError.
+ *
+ * @param jsonFilePath QString Path to a annotation file in which annotation is going to be stored to.
+ * @param imageFilePath QString Path to a image for which annotation is being made.
+ * @param className QString Name of a class which is used for annotating when drawing a shape.
+ * @param coordinates LinkedList of all points in a annotation shape, in format x,y (std::pair<int,int>).
+ */
 void AnnotationModel::add(const QString& jsonFilePath,const QString& imageFilePath, const QString& className, LinkedList<std::pair<int,int>>& coordinates) {
-    QJsonObject annotationDetails;
-    annotationDetails.insert("class",className);
-    QJsonArray points;
+    QFile jsonFile(jsonFilePath); //set file
+    if (!jsonFile.exists()){
+        throw FileNotFoundError();
+    }
+    jsonFile.open(QIODevice::ReadOnly); //open file in read mode
+    QByteArray data = jsonFile.readAll();
+    QJsonDocument json = QJsonDocument::fromJson(data); //create json document from file contents
+    jsonFile.close();
+    QJsonObject allAnnotations = json.object(); //get outer json object
+    QJsonObject newAnnotation; //create new annotation object for storing class name and list of points
+    QJsonArray points; //create array for string points
     for (int i = 0; i < coordinates.length(); i++){
         int x = coordinates.at(i).first;
         int y = coordinates.at(i).second;
         QJsonObject o;
         o.insert("x",x);
         o.insert("y",y);
-        points.append(o);
+        points.append(o); //append object of points to array of points
     }
-    annotationDetails.insert("points",points);
-    QJsonObject annotation;
-    annotation.insert(imageFilePath,annotationDetails);
-    QJsonDocument json;
-    json.setObject(annotation);
-    QFile jsonFile(jsonFilePath);
-    jsonFile.open(QIODevice::WriteOnly | QIODevice::Append);
+    newAnnotation.insert("class",className);
+    newAnnotation.insert("points",points); //fill annotation with data
+    QJsonArray annotationsForThisImage = allAnnotations.value(imageFilePath).toArray(); //get array with all previous annotations for this image
+    annotationsForThisImage.append(newAnnotation); //append all previous annotations with new annotation
+    allAnnotations.insert(imageFilePath,annotationsForThisImage); //set changes to outer json object
+    json.setObject(allAnnotations); //create json document from outer object
+    jsonFile.open(QIODevice::WriteOnly); //write contents to file
     jsonFile.write(json.toJson());
     jsonFile.close();
 }
--- tests/AnnotationModelTests.cpp
+++ tests/AnnotationModelTests.cpp
@@ -4,22 +4,23 @@
 #endif
 #include "../include/AnnotationModel.h"
 #include "../include/LinkedList.h"
+#include "../include/exceptions.h"
 #include <string>
 #include <boost/test/unit_test.hpp>
 
-
+#define TESTFILE "../testFiles/test.annotation"
 BOOST_AUTO_TEST_SUITE( ImageModelTests )
 
     BOOST_AUTO_TEST_CASE(CreatingAnnotationFile){
         //Test that creating annotation file is sucesfull
         AnnotationModel model;
-        BOOST_CHECK_NO_THROW(model.create("../testFiles/testmodel.annotation"));
+        BOOST_CHECK_NO_THROW(model.create(TESTFILE));
     }
 
     BOOST_AUTO_TEST_CASE(CreatingAnnotationFile_notFullName){
         //Tests that creating annotation file, without exstension, is sucesfull
         AnnotationModel model;
-        BOOST_CHECK_NO_THROW(model.create("../testFiles/testmodel"));
+        BOOST_CHECK_NO_THROW(model.create("../testFiles/testWithoutExstenstion"));
     }
 
     BOOST_AUTO_TEST_CASE(CreatingInvalidAnnotationFile){
@@ -32,45 +33,64 @@ BOOST_AUTO_TEST_SUITE( ImageModelTests )
         //Tests that correct active file path is set in internal represenation of 
         //AnnotationModel when creating a new file
         AnnotationModel model;
-        model.create("../testFiles/testmodel.annotation");
-        BOOST_CHECK_EQUAL(model.getCurrentFilePath().toStdString(),"../testFiles/testmodel.annotation");
+        model.create(TESTFILE);
+        BOOST_CHECK_EQUAL(model.getCurrentFilePath().toStdString(),TESTFILE);
     }
 
     BOOST_AUTO_TEST_CASE(CheckCurrentlyActiveFile_notFullName){
         //Tests that correct active file path is set in internal represenation of 
         //AnnotationModel when creating a new file whthout proper extension
         AnnotationModel model;
-        model.create("../testFiles/testmodel");
-        BOOST_CHECK_EQUAL(model.getCurrentFilePath().toStdString(),"../testFiles/testmodel.annotation");
+        model.create("../testFiles/test");
+        BOOST_CHECK_EQUAL(model.getCurrentFilePath().toStdString(),TESTFILE);
     }
 
-    BOOST_AUTO_TEST_CASE(LoadInnotationFile){
+    BOOST_AUTO_TEST_CASE(LoadAnnotationFile){
+        //Tests loading of a annotation file, also checks if it is set as active file
         AnnotationModel model;
-        BOOST_CHECK_NO_THROW(model.browse("../testFiles/template.annotation"));
+        BOOST_CHECK_NO_THROW(model.browse(TESTFILE));
+        BOOST_CHECK_EQUAL(model.getCurrentFilePath().toStdString(),TESTFILE);
     }
 
     BOOST_AUTO_TEST_CASE(LoadInvalidAnnotationFile){
+        //Tests if invalid argument is thrown if empty file name is inputed
         AnnotationModel model;
         BOOST_CHECK_THROW(model.browse(""),std::invalid_argument);
     }
 
-    BOOST_AUTO_TEST_CASE(LoadNonExistingAnnotationFile){
+    BOOST_AUTO_TEST_CASE(LoadAnnotationFile_nonExistingFile){
+        //Tests if file not found exception is thrown if non existing file is provided as parameter
         AnnotationModel model;
-        BOOST_CHECK_THROW(model.browse("../testFiles/templateNOT.annotation"),std::invalid_argument);
+        BOOST_CHECK_THROW(model.browse("../testFiles/nonExisting.annotation"),FileNotFoundError);
     }
 
-    BOOST_AUTO_TEST_CASE(CheckCurrentlyActiveFile_OnLoad){
-        AnnotationModel model;
-        model.browse("../testFiles/template.annotation");
-        BOOST_CHECK_EQUAL(model.getCurrentFilePath().toStdString(),"../testFiles/template.annotation");
-    }
-
-    BOOST_AUTO_TEST_CASE(WriteAnnotation){
+    BOOST_AUTO_TEST_CASE(WriteAnnotationToFile){
+        //Tests if writing annotation to a annotation file is sucesfull
         AnnotationModel model;
         LinkedList<std::pair<int,int>> cords;
         for (int i = 1; i<5;i++)
-            cords.push(std::make_pair(123*i,312*i));
-        model.add("../testFiles/annotations.annotation","image1.png","className2",cords);
+            cords.push(std::make_pair(1223*i,322*i));
+        BOOST_CHECK_NO_THROW(model.add(TESTFILE,"imageExample.png","classExample",cords));
+    }
+
+    BOOST_AUTO_TEST_CASE(WriteAnnotationToFile_FromActiveFile){
+        //Tests if writing annotation to a annotation file is sucesfull
+        //Annotation file path is not provided, instead it is obtained from browsing for a file
+        AnnotationModel model;
+        model.browse(TESTFILE);
+        LinkedList<std::pair<int,int>> cords;
+        for (int i = 1; i<5;i++)
+            cords.push(std::make_pair(1223*i,322*i));
+        BOOST_CHECK_NO_THROW(model.add("imageExample2.png","classExample2",cords));
+    }
+
+    BOOST_AUTO_TEST_CASE(WriteAnnotationToFile_nonExistingFile){
+        //Tests if writting annotation to a non existing file throws file not found error
+        AnnotationModel model;
+        LinkedList<std::pair<int,int>> cords;
+        for (int i = 1; i<5;i++)
+        cords.push(std::make_pair(1223*i,322*i));
+        BOOST_CHECK_THROW(model.add("INVALIDFILE","imageExample2.png","classExample2",cords),FileNotFoundError);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
