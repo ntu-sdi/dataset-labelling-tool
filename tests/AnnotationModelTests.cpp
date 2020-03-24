272a19f0f964b79a667c69c0cdd9b2d5e2b6e995
IcE <ivica-matic@outlook.com>
2020-03-24 00:19:57 +0000

    Written all functions,unit tests and comments.
    Code ready for pair review

--- tests/AnnotationModelTests.cpp
+++ tests/AnnotationModelTests.cpp
@@ -10,23 +10,28 @@
 #include <iostream>
 #include <boost/test/unit_test.hpp>
 
-using Point = QPair<int, int>;
+using Point = QPair<int,int>;
 using Shape = LinkedList<Point>;
+using Annotation = QMap<QString, Shape>;
 
-#define TESTFILE "../testFiles/test.annotation"
-#define TESTFILENOEXSTENSION "../testFiles/test"
-#define TESTFILEINVALIDEXSTENSION "../testFiles/testnoexstension"
-#define IMAGEONE "exampleImage.png"
-#define CLASSONE "car"
-#define CLASSTWO "boat"
-#define IMAGETWO "exampleImage2.jpg"
+Shape createPoints(int multiplier){
+    LinkedList<Point> cords;
+    for (int i = 1; i<5;i++){
+        cords.push(qMakePair(100*i*multiplier,100*i*multiplier));
+    }
+    return cords;
+}
 
-LinkedList<Point> createPoints(){
-        LinkedList<Point> cords;
-        for (int i = 1; i<5;i++){
-            cords.push(std::make_pair(100*i,100*i));
+bool compareShapes(Shape &a, Shape &b){
+    if (a.length() != b.length()) {
+        return false;
+    }
+    for (int i = 0; i< a.length(); i++){
+        if(a.at(i) != b.at(i)){
+            return false;
         }
-        return cords;
+    }
+    return true;
 }
 
 BOOST_AUTO_TEST_SUITE( AnnotationModelTests )
@@ -34,13 +39,7 @@ BOOST_AUTO_TEST_SUITE( AnnotationModelTests )
     BOOST_AUTO_TEST_CASE(CreatingAnnotationFile){
         //Test that creating annotation file is sucesfull
         AnnotationModel model;
-        BOOST_CHECK_NO_THROW(model.create(TESTFILE));
-    }
-
-    BOOST_AUTO_TEST_CASE(CreatingAnnotationFile_notFullName){
-        //Tests that creating annotation file, without exstension, is sucesfull
-        AnnotationModel model;
-        BOOST_CHECK_NO_THROW(model.create(TESTFILEINVALIDEXSTENSION));
+        BOOST_CHECK_NO_THROW(model.create("../testFiles/test.annotation"));
     }
 
     BOOST_AUTO_TEST_CASE(CreatingInvalidAnnotationFile){
@@ -53,23 +52,23 @@ BOOST_AUTO_TEST_SUITE( AnnotationModelTests )
         //Tests that correct active file path is set in internal represenation of 
         //AnnotationModel when creating a new file
         AnnotationModel model;
-        model.create(TESTFILE);
-        BOOST_CHECK_EQUAL(model.getCurrentFilePath().toStdString(),TESTFILE);
+        model.create("../testFiles/test.annotation");
+        BOOST_CHECK_EQUAL(model.getCurrentFilePath().toStdString(),"../testFiles/test.annotation");
     }
 
     BOOST_AUTO_TEST_CASE(CheckCurrentlyActiveFile_notFullName){
         //Tests that correct active file path is set in internal represenation of 
         //AnnotationModel when creating a new file whthout proper extension
         AnnotationModel model;
-        model.create("../testFiles/test");
-        BOOST_CHECK_EQUAL(model.getCurrentFilePath().toStdString(),TESTFILE);
+        model.create("../testFiles/test1");
+        BOOST_CHECK_EQUAL(model.getCurrentFilePath().toStdString(),"../testFiles/test1.annotation");
     }
 
     BOOST_AUTO_TEST_CASE(LoadAnnotationFile){
         //Tests loading of a annotation file, also checks if it is set as active file
         AnnotationModel model;
-        BOOST_CHECK_NO_THROW(model.browse(TESTFILE));
-        BOOST_CHECK_EQUAL(model.getCurrentFilePath().toStdString(),TESTFILE);
+        BOOST_CHECK_NO_THROW(model.browse("../testFiles/test.annotation"));
+        BOOST_CHECK_EQUAL(model.getCurrentFilePath().toStdString(),"../testFiles/test.annotation");
     }
 
     BOOST_AUTO_TEST_CASE(LoadInvalidAnnotationFile){
@@ -86,39 +85,57 @@ BOOST_AUTO_TEST_SUITE( AnnotationModelTests )
 
     BOOST_AUTO_TEST_CASE(WriteAnnotationToFile){
         //Tests if writing annotation to a annotation file is sucesfull
-        AnnotationModel model;
-        LinkedList<QPair<int,int>> cords;
-        for (int i = 1; i<5;i++)
-            cords.push(std::make_pair(100*i,100*i));
-        BOOST_CHECK_NO_THROW(model.add(TESTFILE,IMAGEONE,CLASSONE,cords));
-    }
-
-    BOOST_AUTO_TEST_CASE(WriteAnnotationToFile_FromActiveFile){
-        //Tests if writing annotation to a annotation file is sucesfull
         //Annotation file path is not provided, instead it is obtained from browsing for a file
         AnnotationModel model;
-        model.browse(TESTFILE);
-        LinkedList<QPair<int,int>> cords;
-        for (int i = 1; i<5;i++)
-            cords.push(std::make_pair(100*i,200*i));
-        BOOST_CHECK_NO_THROW(model.add(IMAGETWO,CLASSTWO,cords));
+        model.browse("../testFiles/test.annotation");
+        Shape shape = createPoints(2);
+        BOOST_CHECK_NO_THROW(model.add("imageExampleTwo.png" ,"classExampleTwo",shape));
+    }
+
+    BOOST_AUTO_TEST_CASE(WriteAnnotationToFile_ExternalFile){
+        //Tests if writing annotation to a annotation file is sucesfull
+        AnnotationModel model;
+        Shape shape = createPoints(1);
+        BOOST_CHECK_NO_THROW(model.add("../testFiles/test.annotation","imageExampleOne.png","exampleClassOne",shape));
     }
 
     BOOST_AUTO_TEST_CASE(WriteAnnotationToFile_nonExistingFile){
         //Tests if writting annotation to a non existing file throws file not found error
         AnnotationModel model;
-        LinkedList<QPair<int,int>> cords;
-        for (int i = 1; i<5;i++)
-        cords.push(std::make_pair(100*i,100*i));
-        BOOST_CHECK_THROW(model.add("INVALIDFILE","imageExample2.png","classExample2",cords),FileNotFoundError);
+        Shape shape = createPoints(1);
+        BOOST_CHECK_THROW(model.add("INVALIDFILE","imageExampleThree.png","classExampleTwo",shape),FileNotFoundError);
     }
 
     BOOST_AUTO_TEST_CASE(ReadAnnotationFile){
-        //Tests if reading annotation file return
+        //Tests if reading annotation file return what was written to it
         AnnotationModel model;
-        model.browse(TESTFILE);
-        QMap <QString,Shape> annotations =  model.get("imageExample.png");
+        model.browse("../testFiles/test.annotation");
+        Annotation readAnnotation = model.get("imageExampleOne.png");
+        Shape x = readAnnotation["exampleClassOne"];
+        Shape y = createPoints(1);
+        BOOST_CHECK(compareShapes(x,y));
+    }
 
+    BOOST_AUTO_TEST_CASE(ReadAnnotationFile_ExternalFile){
+        //Tests if reading annotation file return what was written to it
+        AnnotationModel model;
+        Annotation readAnnotation = model.get("imageExampleTwo.png","../testFiles/test.annotation");
+        Shape x = readAnnotation["classExampleTwo"];
+        Shape y = createPoints(2);
+        BOOST_CHECK(compareShapes(x,y));
+    }
+
+    BOOST_AUTO_TEST_CASE(ReadAnnotationFile_nonAnnotatedImage){
+        //Tests if reading annotation file return what was written to it
+        AnnotationModel model;
+        model.browse("../testFiles/test.annotation");
+        BOOST_CHECK_THROW(model.get("imageExampleThree.png"),ImageNotAnnotatedYet);
+    }
+
+    BOOST_AUTO_TEST_CASE(ReadAnnotationFile_ExternalFile_nonAnnotatedImage){
+        //Tests if reading annotation file return what was written to it
+        AnnotationModel model;
+        BOOST_CHECK_THROW(model.get("imageExampleThree.png","../testFiles/test.annotation"),ImageNotAnnotatedYet);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
