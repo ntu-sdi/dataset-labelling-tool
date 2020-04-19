c73225b8fa6c84b254910fd9ef0a457e71452a6b
Ivica <ivica-matic@outlook.com>
2020-04-19 20:44:06 +0100

    Fixed text  formatting displaying image details

--- src/ImageController.cpp
+++ src/ImageController.cpp
@@ -43,9 +43,9 @@ void ImageController::sortLoaded() {}
 void ImageController::select(const QString& a) {
     QString text;
     text = QString::number(model.getFileSize(a)/1000);
-    text += "kB ";
+    text += " kB ";
     text += QString::number(model.getResolution(a).first);
-    text += "X";
+    text += " x ";
     text += QString::number(model.getResolution(a).second);
     ui.ImageInfoLabel->setText(text);
 
